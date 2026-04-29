ef7e2e225c7c937c57adef698754b967219ecb64
Brysonak <brysonak@protonmail.com>
2026-04-29 19:06:47 -0400

    Add license

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,23 @@
+/*
+    taskfile - Just Another Task Runner
+    Copyright (C) 2026 Bryson Kelly
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    If you have any questions and or concerns, please contact me @ brysonak@protonmail (dot com)
+ */
+
 use std::fmt;
 
 /// All error kinds tsk can produce, with distinct exit codes
